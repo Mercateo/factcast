e2826a3c91551e00c943d6314d139b8f606712fd
Uwe Schaefer <uwe.schaefer@mercateo.com>
2018-10-17 15:49:06 +0200

    #142: adjusted config regarding coverall and lombok

--- factcast-client-cache/src/test/java/org/factcast/client/cache/CachingFactCast0Test.java
+++ factcast-client-cache/src/test/java/org/factcast/client/cache/CachingFactCast0Test.java
@@ -1,8 +1,14 @@
 package org.factcast.client.cache;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
@@ -109,6 +115,13 @@ public class CachingFactCast0Test {
         Exception e = new Exception();
         underlyingObserver.onError(e);
         verify(observer).onError(same(e));
+    }
+
+    @Test
+    public void testSerialOf() throws Exception {
+        UUID id = UUID.randomUUID();
+        uut.serialOf(id);
+        verify(fc).serialOf(same(id));
     }
 
 }
