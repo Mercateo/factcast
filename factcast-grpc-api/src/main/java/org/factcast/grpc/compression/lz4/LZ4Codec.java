9f0cfec85f18eeacfd051c998681167610197dda
Uwe Schaefer <uwe@codesmell.de>
2018-11-15 13:01:54 +0100

    #98: removed @Generated from non-generated code for future java compatibility with jdk11

--- factcast-grpc-api/src/main/java/org/factcast/grpc/compression/lz4/LZ4Codec.java
+++ factcast-grpc-api/src/main/java/org/factcast/grpc/compression/lz4/LZ4Codec.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Mercateo AG (http://www.mercateo.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import io.grpc.Codec;
-import lombok.Generated;
 import net.jpountz.lz4.LZ4BlockInputStream;
 import net.jpountz.lz4.LZ4BlockOutputStream;
 
 // waits for release of https://github.com/yidongnan/grpc-spring-boot-starter/issues/96
 // exclude from coverage analysis
-@Generated
 public class LZ4Codec implements Codec {
 
     public static final String ENCODING = "lz4";
--- factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/catchup/queue/PGQueueCatchUpFactory.java
+++ factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/catchup/queue/PGQueueCatchUpFactory.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Mercateo AG (http://www.mercateo.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,13 +26,11 @@ import org.factcast.store.pgsql.internal.catchup.PGCatchupFactory;
 import org.factcast.store.pgsql.internal.query.PGFactIdToSerialMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import lombok.Generated;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 // no code in here, just generated @NonNull checks
-@Generated
 public class PGQueueCatchUpFactory implements PGCatchupFactory {
 
     @NonNull
