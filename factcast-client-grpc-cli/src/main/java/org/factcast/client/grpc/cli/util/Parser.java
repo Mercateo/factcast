a80c7edfac89d5df898ba94bb8319196f3e341af
Christian Flamm <christian.flamm@mercateo.com>
2018-12-10 16:04:59 +0100

    print usage when cli options incomplete

--- factcast-client-grpc-cli/src/main/java/org/factcast/client/grpc/cli/util/Parser.java
+++ factcast-client-grpc-cli/src/main/java/org/factcast/client/grpc/cli/util/Parser.java
@@ -52,14 +52,15 @@ public class Parser {
     public Command parse(String[] args) {
         jc.parse(args);
 
-        if (options.help) {
-            jc.usage();
-            return null;
+        JCommander parsedCommand = jc.getCommands().get(jc.getParsedCommand());
+        if (parsedCommand != null) {
+            init();
+            List<Object> objects = parsedCommand.getObjects();
+            return (Command) objects.get(0);
         }
+        jc.usage();
+        return null;
 
-        init();
-        List<Object> objects = jc.getCommands().get(jc.getParsedCommand()).getObjects();
-        return (Command) objects.get(0);
     }
 
     private void init() {
